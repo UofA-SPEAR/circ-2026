840f28b349448eae754898303eca458adf2b41aa
nathan-correia <nathanc54@shaw.ca>
2026-07-27 21:10:09 -0600

    plex.urdf.xacro

--- plex_moveit/setup.py
+++ plex_moveit/setup.py
@@ -15,6 +15,7 @@ setup(
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.py'))),
         (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*'))),
         (os.path.join('share', package_name, 'meshes'), glob(os.path.join('meshes', '*'))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
