6138bceab001b69c2c1ec4f3e0117cd0674ea0d4
nathan-correia <nathanc54@shaw.ca>
2026-07-27 20:26:53 -0600

    plex_moveit

--- plex_moveit/setup.py
+++ plex_moveit/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'plex_moveit'
 
@@ -10,6 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.py'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*'))),
+        (os.path.join('share', package_name, 'meshes'), glob(os.path.join('meshes', '*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +29,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'gamepad_to_servo = plex_moveit.gamepad_to_servo:main',
+            'keyboard_to_servo = plex_moveit.keyboard_to_servo:main',
         ],
     },
 )
--- plex_ros2_control/setup.py
+++ plex_ros2_control/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'plex_ros2_control'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.py'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
